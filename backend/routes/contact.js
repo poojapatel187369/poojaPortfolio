1fc58cc3def1436542c696fc759c8c80b6100e75
pooja patel <poojapatel187369@gmail.com>
2025-10-17 22:29:29 +0530

    Update contact.js

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
-import Contact from '../models/contact.js';
+import Contact from '../models/Contact.js';
 
 const router = express.Router();
 
